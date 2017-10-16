b94b712a5706d61da0443333731c4bf14187c2e3
Dhananjay P Shinkar <dps48@afsconnect1.njit.edu>
2017-10-15 21:50:57 -0400

    main class added

--- index.php
+++ index.php
@@ -8,8 +8,10 @@ error_reporting(E_ALL);
 //instantiate the program object
 
 //Class to load classes it finds the file when the progrm starts to fail for calling a missing class
-class Manage {
-    public static function autoload($class) {
+class Manage 
+{
+    public static function autoload($class) 
+    {
         //you can put any file name or directory here
         include $class . '.php';
     }
@@ -19,6 +21,38 @@ spl_autoload_register(array('Manage', 'autoload'));
 
 //instantiate the program object
 $obj = new main();
+
+
+
+class main 
+{
+
+    public function __construct()
+    {
+        //set default page request when no parameters are in URL
+        $pageRequest = 'homepage';
+
+        //check if there are parameters
+        if(isset($_REQUEST['page'])) 
+        {
+            //load the type of page the request wants into page request
+            $pageRequest = $_REQUEST['page'];
+        }
+        //instantiate the class that is being requested
+         $page = new $pageRequest;
+
+
+        if($_SERVER['REQUEST_METHOD'] == 'GET') 
+        {
+            $page->get();
+        } else 
+        {
+            $page->post();
+        }
+
+    }
+
+}
 
 
 ?>
